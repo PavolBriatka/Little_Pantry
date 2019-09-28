dd221d1a03fc784132753ea9988c27be34943fd4
Pavol Briatka <pavol.briatka@gmail.com>
2019-09-28 21:59:55 +0100

    First commit: Define BaseApplication, AppComponent and import libraries (mostly Firebase and Dagger)

---
+++ app/src/main/java/com/briatka/pavol/littlepantry/di/AppComponent.kt
@@ -0,0 +1,26 @@
+package com.briatka.pavol.littlepantry.di
+
+import android.app.Application
+import com.briatka.pavol.littlepantry.BaseApplication
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.AndroidInjector
+import dagger.android.support.AndroidSupportInjectionModule
+import javax.inject.Singleton
+
+@Singleton
+@Component(
+    modules = [AndroidSupportInjectionModule::class,
+        AppModule::class]
+)
+interface AppComponent : AndroidInjector<BaseApplication> {
+
+    @Component.Builder
+    interface Builder {
+
+        @BindsInstance
+        fun application(application: Application): Builder
+
+        fun build(): AppComponent
+    }
+}
---
+++ app/src/main/java/com/briatka/pavol/littlepantry/di/AppModule.kt
@@ -0,0 +1,23 @@
+package com.briatka.pavol.littlepantry.di
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+class AppModule {
+
+    @Singleton
+    @Provides
+    fun provideFirebaseAuthInstance(): FirebaseAuth {
+        return FirebaseAuth.getInstance()
+    }
+
+    @Singleton
+    @Provides
+    fun provideFirebaseDatabseInstance(): FirebaseDatabase {
+        return FirebaseDatabase.getInstance()
+    }
+}
