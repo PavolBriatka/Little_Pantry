01b9464bcdd280d109fa2400d2aeb3a2f6b70cd8
PavolBriatka <pavol.briatka@redcloudtechnology.com>
2020-03-25 22:13:52 +0000

    dev: Mock interaction with Firebase components in unit tests

--- app/src/test/java/com/briatka/pavol/littlepantry/AuthViewModelTest.kt
+++ app/src/test/java/com/briatka/pavol/littlepantry/AuthViewModelTest.kt
@@ -3,29 +3,40 @@ package com.briatka.pavol.littlepantry
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.briatka.pavol.littlepantry.ui.auth.viewmodel.AuthViewModel
 import com.briatka.pavol.littlepantry.ui.auth.viewmodel.UserState
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
-import com.nhaarman.mockito_kotlin.any
-import com.nhaarman.mockito_kotlin.times
-import com.nhaarman.mockito_kotlin.verify
-import org.junit.Assert
+import com.google.firebase.storage.FirebaseStorage
+import io.mockk.*
+import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.mockito.Mock
-import org.mockito.junit.MockitoJUnitRunner
 
-@RunWith(MockitoJUnitRunner::class)
+
 class AuthViewModelTest {
 
     private lateinit var viewModel: AuthViewModel
 
-    @Mock
-    lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var auth: FirebaseAuth
+    private lateinit var database: FirebaseFirestore
+    private lateinit var storage: FirebaseStorage
 
-    @Mock
-    lateinit var firestoreDb: FirebaseFirestore
+    private val stringSlot = mutableListOf<String>()
+    private val taskListenerSlot = slot<OnCompleteListener<AuthResult>>()
+
+    private val mockResultTask = mockk<Task<AuthResult>>()
+
+    /* @Mock
+     lateinit var firebaseAuth: FirebaseAuth
+
+     @Mock
+     lateinit var firestoreDb: FirebaseFirestore
+
+     @Mock
+     lateinit var firebaseStorage: FirebaseStorage*/
 
     @get: Rule
     val rule = InstantTaskExecutorRule()
@@ -33,7 +44,21 @@ class AuthViewModelTest {
     @Before
     fun setup() {
 
-        viewModel = AuthViewModel(firebaseAuth, firestoreDb)
+        every { mockResultTask.isSuccessful } returns false
+
+        mockkStatic(FirebaseAuth::class)
+        every { FirebaseAuth.getInstance() } returns mockk(relaxed = true)
+        auth = FirebaseAuth.getInstance()
+
+        mockkStatic(FirebaseFirestore::class)
+        every { FirebaseFirestore.getInstance() } returns mockk(relaxed = true)
+        database = FirebaseFirestore.getInstance()
+
+        mockkStatic(FirebaseStorage::class)
+        every { FirebaseStorage.getInstance() } returns mockk(relaxed = true)
+        storage = FirebaseStorage.getInstance()
+
+        viewModel = AuthViewModel(auth, database, storage)
     }
 
     @Test
@@ -42,23 +67,63 @@ class AuthViewModelTest {
         //Act
         viewModel.startUserRegistration()
         //Assert
-        Assert.assertTrue(viewModel.userState.value == UserState.StartUserRegistration)
+        assertTrue(viewModel.userState.value == UserState.StartUserRegistration)
     }
 
     @Test
     fun startUserRegistration_createUserMethodInvoked() {
         //Arrange
         prepareCredentials()
+
         //Act
         viewModel.startUserRegistration()
+
         //Assert
-        verify(firebaseAuth, times(1)).createUserWithEmailAndPassword(any(), any())
+        verify { auth.createUserWithEmailAndPassword(capture(stringSlot), capture(stringSlot)) }
+        assertTrue(stringSlot[0] == "a@b.com")
+        assertTrue(stringSlot[1] == "123456789")
     }
+
+    @Test
+    fun startUserRegistration_failure_failedUserState() {
+        //Arrange
+        prepareCredentials()
+        userRegistrationFailureMockInteractions()
+
+        //Act
+        viewModel.startUserRegistration()
+
+        //Assert
+        //First assert then the User state is of type DataUpdateFailed
+        assertTrue(viewModel.userState.value is UserState.DataUpdateFailed )
+        //Then we can cast the state to DataUpdateFailed class and verify the error message
+        val userState = viewModel.userState.value as UserState.DataUpdateFailed
+        assertTrue(userState.error == errorMessage)
+    }
+
+    //region Helpers
 
     private fun prepareCredentials() {
         viewModel.userEmail.onNext("a@b.com")
         viewModel.userPassword.onNext("123456789")
     }
+
+    private fun userRegistrationFailureMockInteractions() {
+
+        every { auth.createUserWithEmailAndPassword(any(), any()) } returns mockResultTask
+        every { mockResultTask.exception }  returns Exception(errorMessage)
+        every { mockResultTask.addOnCompleteListener(capture(taskListenerSlot)) } answers {
+
+            taskListenerSlot.captured.onComplete(mockResultTask)
+            mockResultTask
+        }
+    }
+
+    private val errorMessage = "To err if human"
+
+    //endregion
+
+
 
     /*private fun prepareAuthResponse() {
         Mockito.`when`(firebaseAuth.createUserWithEmailAndPassword(any(), any())).thenReturn(
